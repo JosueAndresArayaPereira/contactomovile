f44e742837ace21030699d2950deecc6efb2407a
josue <josueandres.ap@gmail.com>
2024-12-05 23:08:49 -0300

    creacion de vistas y modificacion de manifest

--- app/src/main/java/com/example/contactomovile/activities/SingUpActivity.java
+++ app/src/main/java/com/example/contactomovile/activities/SingUpActivity.java
@@ -1,4 +1,4 @@
-package com.example.contactomovile;
+package com.example.contactomovile.activities;
 
 import android.os.Bundle;
 
@@ -8,11 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.contactomovile.R;
+
+public class SingUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sing_up);
     }
 }
