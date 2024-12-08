99bbe5285142144a972dca9006a45ca87cc78f5c
josue <josueandres.ap@gmail.com>
2024-12-08 19:28:47 -0300

    funcionalidad Chat

--- app/src/main/java/com/example/contactomovile/activities/ChatActivity.java
+++ app/src/main/java/com/example/contactomovile/activities/ChatActivity.java
@@ -1,22 +1,43 @@
 package com.example.contactomovile.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.contactomovile.R;
+import com.example.contactomovile.adapters.ChatAdapter;
 import com.example.contactomovile.databinding.ActivityChatBinding;
+import com.example.contactomovile.models.ChatMessage;
 import com.example.contactomovile.models.User;
 import com.example.contactomovile.utilities.Constants;
+import com.example.contactomovile.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import android.util.Base64;
+import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class ChatActivity extends AppCompatActivity {
 
     private ActivityChatBinding binding;
     private User recieverUser;
+
+    private List<ChatMessage> chatMessages;
+    private ChatAdapter chatAdapter;
+    private PreferenceManager preferenceManager;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +46,111 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setListeners();
         loadReceiverDetails();
+        init();
+        listenMessages();
     }
 
-    private void loadReceiverDetails(){
+    private void init() {
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        chatMessages = new ArrayList<>();
+        chatAdapter = new ChatAdapter(
+                getBitmapFromEncodedString(recieverUser.image),
+                chatMessages,
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+        database = FirebaseFirestore.getInstance();
+    }
+
+    private void sendMessage() {
+        database = FirebaseFirestore.getInstance();
+        String messageContent = binding.inputMessage.getText().toString().trim();
+        if (messageContent.isEmpty()) {
+            // Muestra un mensaje de error al usuario
+            binding.inputMessage.setError("El mensaje no puede estar vacío");
+            return;
+        }
+
+        HashMap<String, Object> message = new HashMap<>();
+        message.put(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID));
+        message.put(Constants.KEY_RECEIVER_ID, recieverUser.id);
+        message.put(Constants.KEY_MESSAGE, messageContent);
+        message.put(Constants.KEY_TIMESTAMP, new Date());
+
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .add(message)
+                .addOnSuccessListener(documentReference -> {
+                    // Mensaje enviado con éxito
+                    binding.inputMessage.setText(null);
+                })
+                .addOnFailureListener(e -> {
+                    // Manejar el error
+                    e.printStackTrace();
+                });
+    }
+
+    private void listenMessages(){
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, recieverUser.id)
+                .addSnapshotListener(eventListener);
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID,recieverUser.id)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+    }
+
+    private final EventListener<QuerySnapshot> eventListener = (value, error) -> {
+        if (error != null) {
+            return;
+        }
+
+        if (value != null) {
+            int count = chatMessages.size();
+            for (DocumentChange documentChange : value.getDocumentChanges()) {
+                if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                    ChatMessage chatMessage = new ChatMessage();
+                    chatMessage.senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                    chatMessage.receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                    chatMessage.message = documentChange.getDocument().getString(Constants.KEY_MESSAGE);
+                    chatMessage.dateTime = getReadableDateTime(documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP));
+                    chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                    chatMessages.add(chatMessage);
+                }
+            }
+
+            // Ordenar mensajes por fecha
+            Collections.sort(chatMessages, (obj1, obj2) -> obj1.dateObject.compareTo(obj2.dateObject));
+
+            if (count == 0) {
+                chatAdapter.notifyDataSetChanged();
+            } else {
+                chatAdapter.notifyItemRangeInserted(count, chatMessages.size() - count);
+                binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size() - 1);
+            }
+
+            binding.chatRecyclerView.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.GONE);
+        }
+    };
+
+
+    private Bitmap getBitmapFromEncodedString(String encodedImage) {
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    private void loadReceiverDetails() {
         recieverUser = (User) getIntent().getSerializableExtra(Constants.KEY_USER);
         binding.textName.setText(recieverUser.name);
     }
-    private void setListeners(){
+
+    private void setListeners() {
         binding.imageBack.setOnClickListener(v -> onBackPressed());
+        binding.layoutSend.setOnClickListener(v -> sendMessage());
+    }
+
+    private String getReadableDateTime(Date date){
+        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
     }
 }
